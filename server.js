cdb8beca2e412f1070d758bbd07e2a48e1be5fdc
Prajwalg19 <theimerpical8@gmail.com>
2023-11-01 16:11:56 +0530

    add mongoose and connect to it

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connected");
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+const PORT = process.env.PORT_NUMBER || 4500;
+
+app.listen(PORT, () => {
+    console.log("Server Stared at port :", PORT);
+});
+
+app.use("/", (req, res) => {
+    res.send();
+});
