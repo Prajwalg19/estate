0b9eae481354c53ab1a83f201181e757da37f7e7
Prajwalg19 <theimperical8@gmail.com>
2024-03-30 20:05:23 +0530

    cors

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const settings = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: "https://estate-lake.vercel.app/",
     withCredentials: true,
 });
 
