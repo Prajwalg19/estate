de2e319302b5c6a8e31109b5297c760694fad57c
Prajwalg19 <theimperical8@gmail.com>
2024-03-30 21:36:21 +0530

    cors

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,16 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    // Remove or comment out the proxy configuration
-    // server: {
-    //     proxy: {
-    //         '/api': {
-    //             target: 'http://localhost:4000',
-    //             secure: false,
-    //         },
-    //     },
-    // },
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://localhost:4000",
+                secure: false,
+            },
+        },
+    },
 
     plugins: [react()],
-})
+});
