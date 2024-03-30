d1f06f6a430ec169d4607d64a1f6cad4e3fac45c
Prajwalg19 <theimperical8@gmail.com>
2024-03-30 22:36:11 +0530

    page not found

--- api/server.js
+++ api/server.js
@@ -9,11 +9,6 @@ import cookieParser from "cookie-parser";
 import path from "path";
 dotenv.config();
 const app = express();
-const corsOpts = {
-    origin: ["https://estate-lake.vercel.app", "http://localhost:5173"],
-    credentials: true,
-};
-app.use(cors(corsOpts));
 mongoose
     .connect(process.env.MONGO)
     .then(() => {
@@ -23,18 +18,24 @@ mongoose
         console.log(e);
     });
 const PORT = process.env.PORT_NUMBER || 4500;
-app.use(express.json());
-app.use(cookieParser());
-const dirname = path.resolve();
 app.listen(PORT, () => {
     console.log("Server Stared at port :", PORT);
 });
+const corsOpts = {
+    origin: ["https://estate-lake.vercel.app", "http://localhost:5173"],
+    credentials: true,
+};
+app.use(cors(corsOpts));
+
+app.use(express.json());
+app.use(cookieParser());
+const __dirname = path.resolve();
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listings", listingRouter);
-app.use(express.static(path.join(dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("*", (req, res) => {
-    res.sendFile(path.join(dirname, "client", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 app.use((err, req, res, next) => {
     let statusCode = err.statusCode || 500;
