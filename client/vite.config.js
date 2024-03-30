dfd413d9106be6089d18291cca4a74f9787efefb
Prajwalg19 <theimperical8@gmail.com>
2024-03-30 21:50:01 +0530

    cors

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,11 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
         proxy: {
-            "/api": {
+            "/": {
                 target: "http://localhost:4000",
                 secure: false,
             },
