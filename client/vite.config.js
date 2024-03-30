03573d53225af8211311a394c58d110a1f41be0d
Prajwalg19 <theimperical8@gmail.com>
2024-03-30 20:47:31 +0530

    cors

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://localhost:4000",
+                target: "https://estate-lake.vercel.app/",
                 secure: false,
             },
         },
