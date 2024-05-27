66f14883435ffe6682c3dce777522096499acadd
Prajwal G <93521144+Prajwalg19@users.noreply.github.com>
2024-05-27 05:38:01 +0000

    pacman spinner

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,8 +1,8 @@
-import { MoonLoader } from "react-spinners";
+import { PacmanLoader } from "react-spinners";
 function Spinner() {
     return (
         <div className="flex justify-center absolute items-center top-0 right-0 left-0 bottom-0 z-50 ">
-            <MoonLoader />{" "}
+            <PacmanLoader  />
         </div>
     );
 }
